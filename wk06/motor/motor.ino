0ef95acf536dc00bcf476890ca5a489321cef85a
pocket-98 <dayalpavan@gmail.com>
2025-02-28 14:00:08 -0700

    motor buffet

--- wk06/motor/motor.ino
+++ wk06/motor/motor.ino
@@ -1,45 +1,60 @@
-// motor.ino
-
-//L293D motor controller
-// https://www.ti.com/lit/ds/symlink/l293d.pdf
-
-// ANALOG SERVO
-//#define PIN 9
-//#define DT 200
-//
-//void setup() {
-//    pinMode(PIN, OUTPUT);
-//}
-//
-//void loop() {
-//    int i;
-//    for (i=0; i<256; i+=10) {
-//        analogWrite(PIN, i);
-//        delay(DT);
-//    }
-//    for (i=255; i>=0; i-=10) {
-//        analogWrite(PIN, i);
-//        delay(DT);
-//    }
-//}
-
-// Stepper Motor
 #include <Stepper.h>
 
-const int stepsPerRevolution = 2038;
+// Define stepper motor steps per revolution
+#define STEPS 60
 
-Stepper s = Stepper(stepsPerRevolution, 8, 10, 9, 11);
+// Create stepper motor instance
+Stepper stepper(STEPS, 8, 10, 9, 11);
+
+// Define button pins
+const int leftButtonPin = 2;
+const int rightButtonPin = 3;
+const int fanPin = 5;
+
+// // Variables to track button state
+// bool leftState = HIGH;
+// bool lastLeftState = HIGH;
+// bool rightState = HIGH;
+// bool lastRightState = HIGH;
+
+// Track motor direction
+int direction = 0; // 1 = clockwise, -1 = counterclockwise
 
 void setup() {
-    //
+    Serial.begin(115200);
+    stepper.setSpeed(600); // Set speed in RPM
+    
+    pinMode(leftButtonPin, INPUT_PULLUP);
+    pinMode(rightButtonPin, INPUT_PULLUP);
+
+    Serial.println("Stepper motor button control initialized.");
 }
 
 void loop() {
-    s.setSpeed(5);
-    s.step(stepsPerRevolution);
-    delay(1000);
-    // Rotate CCW quickly at 10 RPM
-    s.setSpeed(10);
-    s.step(-stepsPerRevolution);
-    delay(1000);
+    // Read button states
+    int leftState = digitalRead(leftButtonPin);
+    int rightState = digitalRead(rightButtonPin);
+
+    // Check for left button press (state change from HIGH to LOW)
+    if (leftState == HIGH && rightState == LOW) {
+        direction = -1; // Set direction to counterclockwise
+        Serial.println("moving LEFT");
+    } else if (rightState == HIGH && leftState == LOW) {
+        direction = 1; // Set direction to clockwise
+        Serial.println("moving RIGHT");
+    } else {
+        direction = 0;
+    }
+
+    // Move motor in the selected direction
+    if (direction == 1) {
+        stepper.step(STEPS * 1); // Move clockwise
+    } else if (direction == -1) {
+        stepper.step(-STEPS * 1); // Move counterclockwise
+    } else {
+        // dont move
+    }
+
+    digitalWrite(fanPin, HIGH);
+
 }
